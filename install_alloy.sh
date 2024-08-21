db457d18e9ce92eee65effca4a07c733db631ca0
Taufiq Permana <35789663+taufiqpsumarna@users.noreply.github.com>
2024-08-21 10:54:50 +0000

    add alloy installation script

---
+++ install_alloy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+ALLOY_CONFIG_FILE=/etc/alloy/config.alloy
+ALLOY_SERVICE_FILE=/etc/systemd/system/alloy.service
+
+echo "Remove existing monitoring tools"
+apt remove prom* alloy*
+apt autoremove -y
+systemctl daemon-reload
+
+echo "Install prerequisite package and add grafana repository"
+apt-get install -y gpg unzip wget curl
+mkdir -p /etc/apt/keyrings/
+wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | tee /etc/apt/keyrings/grafana.gpg > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | tee /etc/apt/sources.list.d/grafana.list
+apt-get update
+
+echo "Install Grafana Alloy"
+apt-get install -y alloy
+
+echo "Copy config.alloy to $ALLOY_CONFIG_DIR"
+cp $ALLOY_CONFIG_DIR $ALLOY_CONFIG_DIR/config.alloy.bak
+cp .config/alloy/config.alloy $ALLOY_CONFIG_DIR
+
+echo "Enable docker integration"
+usermod -a -G docker alloy
+sed -i 's/User=alloy/User=root/' "$ALLOY_SERVICE_FILE"
+systemctl daemon-reload
+systemctl restart alloy
+
+echo "Enabling Grafana alloy"
+systemctl-daemon reload
+systemctl enable alloy
+systemctl start alloy
+systemctl status alloy
